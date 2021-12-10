0fdcbffab96bc29f9b7abd271196bd527a1d5687
lvance <lvance@twilio.com>
2021-12-10 18:44:18 -0500

    replaced cyoa-reader with parkmeow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Grommet, Markdown, Paragraph, ResponsiveContext, Text } from 'grommet';
-
-import FinalForm from './FinalForm';
-
-const storyBlocks = require('./story-blocks.json');
-const storyPaths = require('./story-paths.json');
+import { Box, Grommet, Heading, Paragraph, ResponsiveContext, Text, Form, FormField, Clock, Button } from 'grommet';
 
 const theme = {
   global: {
@@ -21,18 +16,70 @@ const theme = {
 };
 
 function App() {
-  const [currPosition, setCurrPosition] = useState(0);
+  const [value, setValue] = useState(0);
+  const [phone, setPhone] = useState(0);
+  const [verifyStatus, setVerifyStatus] = useState(null);
+  const [authToken, setAuthToken] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-  const currBlock = storyBlocks.find(block => block.position === currPosition);
+  function verifyNumber(data) {
+    setPhone(data.phone);
+    fetch('https://53b8ae39a3ec.ngrok.io/send-code', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      setVerifyStatus(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
 
-  const updatePosition = (currPosition, action) => {
-    const validPaths = storyPaths.filter(path => path.source === currPosition);
-    const targetPosition = validPaths.find(path => path.action === action);
-    if (targetPosition && targetPosition.target >= 0) {
-      setCurrPosition(targetPosition.target);
-    } else {
-      setCurrPosition(-1);
-    }
+  function verifyCode(data) {
+    data.phone = phone;
+    console.log(data);
+    fetch('https://53b8ae39a3ec.ngrok.io/verify-code', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      setAuthToken(data.auth_token);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  function createSession(data) {
+    console.log(data);
+    data.auth_token = authToken;
+    data.amount = data.amount * 1;
+    fetch('https://53b8ae39a3ec.ngrok.io/parking-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      setSuccess(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }
 
   return (
@@ -49,46 +96,55 @@ function App() {
                   align='center'
                   justify='center'
                   elevation='small'
+                  margin={{top: topSpacing}}
                 >
-                  <Paragraph 
-                    margin={{top: topSpacing, bottom: 'medium', horizontal: horizontalSpacing}} 
-                    size='large'
-                    align='justify'
-                  >
-                    <Markdown>
-                      {currBlock.text}
-                    </Markdown>
+                  <Heading> Welcome to Park Right Meow! </Heading>
+                  <Paragraph>
+                    Verify your phone number, then pay right here, no download needed.
                   </Paragraph>
+                  {!verifyStatus && !authToken ?
+                    // Starting form
+                    <Form 
+                      onSubmit={({ value }) => verifyNumber(value)}
+                    >
+                      <FormField name="phone" label="Phone Number" required={true} />
+                      <Button type="submit" label="Send Code" primary={true} padding={{bottom: 'medium'}}/>
+                    </Form> : null
+                  }
+                  {verifyStatus && !authToken ?
+                    // verify code form
+                    <Form 
+                      onSubmit={({ value }) => verifyCode(value)}
+                    >
+                      <FormField name="code" label="SMS Code" required={true} />
+                      <Button type="submit" label="Verify" primary={true} padding={{bottom: 'medium'}}/>
+                    </Form>
+                    : null}
+                  {authToken && !success ?  
+                  // create parking session form
+                  <Form 
+                    onSubmit={({ value }) => createSession(value)}
+                  >
+                    <FormField name="license_plate" label="License Plate" required={true} />
+                    <FormField name="location_code" label="Location Code" required={true} />
+                    <FormField name="amount" label="Time to add (hours)" required={true} />
+                    <Button type="submit" label="Add Time" primary={true} padding={{bottom: 'medium'}}/>
+                  </Form>
+                  : null
+                }
+                {authToken && success ? 
+                <div>
+                  <Paragraph>
+                    Success! Your parking expires in {success[0].amount} hours.
+                  </Paragraph>
+                  <Clock type="digital" />
+                </div>
+                : null}
+                
                 </Box>
               </Box>
-              {currBlock.actions[0] === "END" ? (<FinalForm />) : currBlock.actions.map(action => {
-                return (
-                  <Box
-                    direction='row' 
-                    overflow={{ horizontal: 'hidden' }}
-                    margin={{ bottom: 'medium', horizontal: horizontalSpacing }}
-                    align='center'
-                    justify='center'
-                  >
-                    <Box
-                      flex
-                      align='center'
-                      justify='center'
-                      background='button'
-                      elevation='small'
-                      pad='medium'
-                      onClick={() => updatePosition(currPosition, action)}
-                    >
-                      <Text size='large' color='white'>
-                        {action}
-                      </Text>
-                    </Box>
-                  </Box>
-                );
-              })}
             </Box>
-          )
-        }
+          )}
         }
       </ResponsiveContext.Consumer>
     </Grommet>
